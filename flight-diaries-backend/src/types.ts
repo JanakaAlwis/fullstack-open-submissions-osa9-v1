407aa5973295128af441b0b469fe552873cdf17e
JanakaAlwis <janaka.donmanuwelge@student.hamk.fi>
2025-07-06 11:50:40 +0300

    Revert "Exercises 9.21 - 9.22  done"
    This reverts commit 39c78b36647119bbddc5ffe7f83774b136ac73c6.

--- flight-diaries-backend/src/types.ts
+++ flight-diaries-backend/src/types.ts
@@ -22,25 +22,5 @@ export interface DiaryEntry {
 }
 
 export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
+
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
-
-export enum Gender {
-  Male = 'male',
-  Female = 'female',
-  Other = 'other'
-}
-
-
-export interface Entry {}
-
-export interface Patient {
-  id: string;
-  name: string;
-  ssn: string;
-  occupation: string;
-  gender: Gender;
-  dateOfBirth: string;
-  entries: Entry[];
-}
-
-export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
